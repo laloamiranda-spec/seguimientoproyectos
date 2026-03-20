3bf5b95ddd224206d05ee84bac3b216247d411c7
laloamiranda-spec <lalo.amiranda@gmail.com>
2026-03-19 23:13:05 -0600

    perfil

--- app.py
+++ app.py
@@ -1945,4 +1945,6 @@ def cargar_layout():
 if __name__ == '__main__':
     if not use_pg:
         init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port  = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_ENV', 'production') != 'production'
+    app.run(debug=debug, host='0.0.0.0', port=port)
